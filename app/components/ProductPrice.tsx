87de40e8cffc6a805ec06ea2caf20415f25d407b
Harpreet Singh <harpreetsingh@JustGroup.Internal>
2026-03-24 21:58:37 +1100

    Loyalty points on PDP

--- app/components/ProductPrice.tsx
+++ app/components/ProductPrice.tsx
@@ -10,18 +10,27 @@ export function ProductPrice({
 }) {
   return (
     <div className="product-price">
-      {compareAtPrice ? (
-        <div className="product-price-on-sale">
-          {price ? <Money data={price} /> : null}
-          <s>
-            <Money data={compareAtPrice} />
-          </s>
-        </div>
-      ) : price ? (
-        <Money data={price} />
-      ) : (
-        <span>&nbsp;</span>
-      )}
+  {/* Rewards Section */}
+  {price?.amount && (
+    <p className="rewards-text">
+      Peter's Dreamers Rewards You could earn {Math.floor(parseFloat(price.amount))} Points. 
+      <a href="/account/login"> Sign In</a> or <a href="/account/register"> Join</a>
+    </p>
+  )}
+
+  {/* Existing Price Logic */}
+  {compareAtPrice ? (
+    <div className="product-price-on-sale">
+      {price ? <Money data={price} /> : null}
+      <s>
+        <Money data={compareAtPrice} />
+      </s>
     </div>
+  ) : price ? (
+    <Money data={price} />
+  ) : (
+    <span>&nbsp;</span>
+  )}
+</div>
   );
 }
